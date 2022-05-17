3231937c01361e939dc508d6a0b163b4c353b488
rxchard <40161763+rxchard@users.noreply.github.com>
2022-05-17 21:58:31 +0200

    refactor: add event priority to listeners

--- Plugin/src/main/java/de/corneliusmay/silkspawners/plugin/listeners/SpawnerBreakListener.java
+++ Plugin/src/main/java/de/corneliusmay/silkspawners/plugin/listeners/SpawnerBreakListener.java
@@ -3,11 +3,12 @@ package de.corneliusmay.silkspawners.plugin.listeners;
 import de.corneliusmay.silkspawners.api.SpawnerBreakEvent;
 import de.corneliusmay.silkspawners.plugin.SilkSpawners;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 
 public class SpawnerBreakListener implements Listener {
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGHEST)
     public void onSpawnerBreak(SpawnerBreakEvent e) {
         int intensity = SilkSpawners.getInstance().getPluginConfig().getSpawnerExplosionSilktouch();
         if(intensity == 0) return;
