06e4423c43fffb5be829ab961feb2e203dcff7da
Cornelius May <30781324+CorneliusMa@users.noreply.github.com>
2022-05-23 23:55:56 +0200

    refactor: made dollar char in config escapable

--- Plugin/src/main/java/de/corneliusmay/silkspawners/plugin/config/formatters/MessageConfigValue.java
+++ Plugin/src/main/java/de/corneliusmay/silkspawners/plugin/config/formatters/MessageConfigValue.java
@@ -5,6 +5,6 @@ import de.corneliusmay.silkspawners.plugin.config.handler.ConfigValueFormatter;
 public class MessageConfigValue implements ConfigValueFormatter<String> {
     @Override
     public String format(String value) {
-        return value.replace("$", "§");
+        return value.replaceAll("(?<!\\\\)\\$" , "§").replace("\\$", "$");
     }
 }
