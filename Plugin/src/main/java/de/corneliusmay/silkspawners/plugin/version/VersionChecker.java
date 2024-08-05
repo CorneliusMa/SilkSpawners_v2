3bddc5de60d5234d3a72ad81247e083a8d3f7218
CorneliusMa <30781324+CorneliusMa@users.noreply.github.com>
2024-08-05 20:34:33 +0200

    refactor: use gitlab api to check verison

--- Plugin/src/main/java/de/corneliusmay/silkspawners/plugin/version/VersionChecker.java
+++ Plugin/src/main/java/de/corneliusmay/silkspawners/plugin/version/VersionChecker.java
@@ -12,6 +12,8 @@ import java.util.Arrays;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class VersionChecker {
 
@@ -58,9 +60,16 @@ public class VersionChecker {
 
     private boolean updateLatestVersion() {
         try {
-            HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://storage.googleapis.com/silkspawners/version.html")).GET().build();
-            latestVersion = client.send(request, HttpResponse.BodyHandlers.ofString()).body().replace("\r", ""). replace("\n", "");
-            return true;
+            Pattern pattern = Pattern.compile("\"tag_name\": \"v([0-9\\.]+)\"");
+            HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://api.github.com/repos/CorneliusMa/SilkSpawners_v2/releases/latest")).GET().build();
+            String latestVersionData = client.send(request, HttpResponse.BodyHandlers.ofString()).body();
+            Matcher matcher = pattern.matcher(latestVersionData);
+            if (matcher.find()) {
+                latestVersion = matcher.group(1);
+                return true;
+            } else {
+                return false;
+            }
         } catch (IOException | InterruptedException ex) {
             ex.printStackTrace();
             return false;
@@ -72,6 +81,7 @@ public class VersionChecker {
         Integer[] latestVersion = castVersionString(this.latestVersion);
 
         for(int i = 0; i < latestVersion.length; i++) {
+            if(i >= installedVersion.length) return true;
             if(latestVersion[i] > installedVersion[i]) return false;
         }
 
