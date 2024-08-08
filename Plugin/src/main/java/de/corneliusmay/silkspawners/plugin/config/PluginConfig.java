1b96c9683347b91ff3f5983691e8b532635d9bfa
CorneliusMa <30781324+CorneliusMa@users.noreply.github.com>
2024-08-08 23:02:05 +0200

    feat(config): add config version
    This commit fixes some typos and allows automated config upgrades

--- Plugin/src/main/java/de/corneliusmay/silkspawners/plugin/config/PluginConfig.java
+++ Plugin/src/main/java/de/corneliusmay/silkspawners/plugin/config/PluginConfig.java
@@ -11,18 +11,15 @@ import java.util.Locale;
 
 public enum PluginConfig {
     MESSAGE_PREFIX(ConfigScope.MESSAGES, "prefix", "$8[$bSilkSpawners$8]", new MessageConfigValue()),
-    MESSAGE_LOCALE(ConfigScope.MESSAGES, "lcoale", "en", (ConfigValueFormatter<Locale>) Locale::forLanguageTag),
+    MESSAGE_LOCALE(ConfigScope.MESSAGES, "locale", "en", (ConfigValueFormatter<Locale>) Locale::forLanguageTag, new String[]{ConfigScope.MESSAGES.getPath() + "lcoale"}),
     SPAWNER_DROP_CHANCE(ConfigScope.SPAWNER, "dropChance", 100, new IntegerConfigValue()),
     SPAWNER_DESTROYABLE(ConfigScope.SPAWNER, "destroyable", true, new BooleanConfigValue()),
     SPAWNER_PICKAXE_REQUIRED(ConfigScope.SPAWNER, "pickaxeRequired", true, new BooleanConfigValue()),
     SPAWNER_SILKTOUCH_REQUIRED(ConfigScope.SPAWNER, "silktouchRequired", true, new BooleanConfigValue()),
     SPAWNER_ITEM_NAME(ConfigScope.SPAWNER_ITEM, "name", "$dSpawner", new MessageConfigValue()),
-    SPAWNER_ITEM_PREFIX(ConfigScope.SPAWNER_ITEM, "prefix", "$e", (ConfigValueFormatter<String>) value -> {
-        if(value.equals("")) return "§f";
-        return new MessageConfigValue().format(value);
-    }),
-    SPAWNER_ITEM_PREFIX_OLD(ConfigScope.SPAWNER_ITEM, "prefix-old", "", new MessageConfigValue()),
-    SPAWNER_ITEM_LORE(ConfigScope.SPAWNER_ITEM, "lore", new String[0], new MessageConfigValue()),
+    SPAWNER_ITEM_PREFIX(ConfigScope.SPAWNER_ITEM, "prefix", "$e", value -> value.isEmpty() ? "§f" : new MessageConfigValue().format(value)),
+    SPAWNER_ITEM_PREFIX_OLD(ConfigScope.SPAWNER_ITEM, "prefixOld", "", new MessageConfigValue(), new String[]{ConfigScope.SPAWNER_ITEM.getPath() + "prefix-old"}),
+    SPAWNER_ITEM_LORE(ConfigScope.SPAWNER_ITEM, "lore", new Object[]{new String[0], new String[0]}, new MessageConfigValue()),
     SPAWNER_EXPLOSION_NORMAL(ConfigScope.SPAWNER_EXPLOSION, "normal", 0, new IntegerConfigValue()),
     SPAWNER_EXPLOSION_SILKTOUCH(ConfigScope.SPAWNER_EXPLOSION, "silktouch", 0, new IntegerConfigValue()),
     SPAWNER_MESSAGE_DENY_DESTROY(ConfigScope.SPAWNER_MESSAGES, "denyDestroy", true, new BooleanConfigValue()),
@@ -31,9 +28,12 @@ public enum PluginConfig {
     SPAWNER_PERMISSION_DISABLE_DESTROY(ConfigScope.SPAWNER_PERMISSIONS, "disableDestroy", false, new BooleanConfigValue()),
     SPAWNER_PERMISSION_DISABLE_PLACE(ConfigScope.SPAWNER_PERMISSIONS, "disablePlace", false, new BooleanConfigValue()),
     SPAWNER_PERMISSION_DISABLE_CHANGE(ConfigScope.SPAWNER_PERMISSIONS, "disableChange", false, new BooleanConfigValue()),
+    UPDATE_CONFIG_VERSION(ConfigScope.UPDATE, "configVersion", 2, new IntegerConfigValue()),
     UPDATE_CHECK_ENABLED(ConfigScope.UPDATE_CHECK, "enabled", true, new BooleanConfigValue()),
     UPDATE_CHECK_INTERVAL(ConfigScope.UPDATE_CHECK, "interval", 24, new IntegerConfigValue()),
     ;
+
+    public static final int CONFIG_VERSION = 2;
 
     @Getter
     private FileConfiguration config;
@@ -44,16 +44,43 @@ public enum PluginConfig {
     @Getter
     private final ConfigValueFormatter<?> formatter;
 
-    private final Object defaultValue;
+    private final Object[] defaultValue;
+
+    private String[] legacyKeys;
 
     PluginConfig(ConfigScope scope, String key, Object defaultValue, ConfigValueFormatter<?> formatter) {
+        this.path = scope.getPath() + key;
+        this.defaultValue = new Object[CONFIG_VERSION];
+        this.defaultValue[CONFIG_VERSION - 1] = defaultValue;
+        this.formatter = formatter;
+    }
+
+    PluginConfig(ConfigScope scope, String key, Object[] defaultValue, ConfigValueFormatter<?> formatter) {
         this.path = scope.getPath() + key;
         this.defaultValue = defaultValue;
         this.formatter = formatter;
     }
 
-    public void init(FileConfiguration config) {
+    PluginConfig(ConfigScope scope, String key, Object defaultValue, ConfigValueFormatter<?> formatter, String[] legacyKeys) {
+        this.path = scope.getPath() + key;
+        this.defaultValue = new Object[CONFIG_VERSION];
+        this.defaultValue[CONFIG_VERSION - 1] = defaultValue;
+        this.formatter = formatter;
+        this.legacyKeys = legacyKeys;
+    }
+
+    public void init(FileConfiguration config, Integer initialVersion) {
         this.config = config;
-        config.addDefault(path, defaultValue);
+
+        if (legacyKeys != null && legacyKeys.length > 0 && initialVersion != CONFIG_VERSION) {
+            int legacyKeyIndex = legacyKeys.length >= initialVersion ? initialVersion : legacyKeys.length;
+            String legacyKey = legacyKeys[legacyKeyIndex - 1];
+            config.addDefault(path, config.get(legacyKey));
+            config.set(legacyKey, null);
+        } else {
+            int defaultValueIndex = defaultValue.length >= initialVersion ? initialVersion : defaultValue.length;
+            Object defaultValue = this.defaultValue[defaultValueIndex - 1];
+            config.addDefault(path, defaultValue);
+        }
     }
 }
--- Plugin/src/main/java/de/corneliusmay/silkspawners/plugin/config/handler/ConfigLoader.java
+++ Plugin/src/main/java/de/corneliusmay/silkspawners/plugin/config/handler/ConfigLoader.java
@@ -1,8 +1,14 @@
 package de.corneliusmay.silkspawners.plugin.config.handler;
 
 import de.corneliusmay.silkspawners.plugin.config.PluginConfig;
+import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.Plugin;
+
+import java.io.File;
+import java.util.logging.Level;
+
+import static de.corneliusmay.silkspawners.plugin.config.PluginConfig.CONFIG_VERSION;
 
 public class ConfigLoader {
 
@@ -15,13 +21,41 @@ public class ConfigLoader {
         this.config = plugin.getConfig();
     }
 
+    private int getConfigVersion() {
+        File configFile = new File(plugin.getDataFolder(), "config.yml");
+        if (!configFile.exists()) {
+            Bukkit.getLogger().log(Level.INFO, "[SilkSpawners] No config file was found. The config will be generated with the default configuration");
+            return CONFIG_VERSION;
+        }
+
+        int currentVersion = config.getInt("update.configVersion");
+        if (currentVersion == 0) currentVersion = 1;
+        if (CONFIG_VERSION > currentVersion) {
+            if (CONFIG_VERSION - currentVersion > 1) {
+                Bukkit.getLogger().log(Level.WARNING, "[SilkSpawners] Configuration file in version " + currentVersion + " is outdated. Since the  " + CONFIG_VERSION);
+            } else {
+                config.set("update.configVersion", CONFIG_VERSION);
+                Bukkit.getLogger().log(Level.WARNING, "[SilkSpawners] Configuration file in version " + currentVersion + " is automatically converted to the latest version " + CONFIG_VERSION);
+            }
+        } else {
+            Bukkit.getLogger().log(Level.INFO, "[SilkSpawners] Configuration is up to date");
+        }
+
+        return currentVersion;
+    }
+
     public void load() {
-        for(PluginConfig value : PluginConfig.values()) value.init(config);
+        Bukkit.getLogger().log(Level.INFO, "[SilkSpawners] Loading configuration...");
+
+        int configVersion = getConfigVersion();
+        for (PluginConfig value : PluginConfig.values()) {
+            value.init(config, configVersion);
+        }
         config.options().copyDefaults(true);
         plugin.saveConfig();
         plugin.reloadConfig();
 
-        for(PluginConfig value : PluginConfig.values()) {
+        for (PluginConfig value : PluginConfig.values()) {
             try {
                 new ConfigValue<>(value).get();
             } catch (Exception ex) {
