af0aabc28f5843843334e04c59119292b1e6b976
rxchard <40161763+rxchard@users.noreply.github.com>
2026-07-12 00:43:59 +0200

    refactor: lower API floor to 1.8, centralize it, rename v1_8_4 to v1_8

--- API/build.gradle.kts
+++ API/build.gradle.kts
@@ -1,5 +1,5 @@
 group = "de.corneliusmay.silkspawners"
 
 dependencies {
-    compileOnly("org.bukkit:bukkit:1.13-R0.1-SNAPSHOT")
+    compileOnly(providers.gradleProperty("bukkit").get())
 }
--- PlatformBukkit/build.gradle.kts
+++ PlatformBukkit/build.gradle.kts
@@ -3,5 +3,5 @@ group = "de.corneliusmay.silkspawners"
 dependencies {
     implementation(project(":API"))
 
-    compileOnly("org.bukkit:bukkit:1.13-R0.1-SNAPSHOT")
+    compileOnly(providers.gradleProperty("bukkit").get())
 }
