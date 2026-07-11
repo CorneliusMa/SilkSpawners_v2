9f82a65cd326c3c245291a395598ef7ecd679328
rxchard <40161763+rxchard@users.noreply.github.com>
2026-07-11 19:20:12 +0200

    feat: add MiniMessage support
    Resolves: #44

--- Plugin/src/main/java/de/corneliusmay/silkspawners/plugin/config/formatters/MessageConfigValue.java
+++ Plugin/src/main/java/de/corneliusmay/silkspawners/plugin/config/formatters/MessageConfigValue.java
@@ -1,10 +1,11 @@
 package de.corneliusmay.silkspawners.plugin.config.formatters;
 
 import de.corneliusmay.silkspawners.plugin.config.handler.ConfigValueFormatter;
+import de.corneliusmay.silkspawners.plugin.utils.MessageRenderer;
 
 public class MessageConfigValue implements ConfigValueFormatter<String> {
     @Override
     public String format(String value) {
-        return value.replaceAll("(?<!\\\\)\\$" , "§").replace("\\$", "$");
+        return MessageRenderer.render(value.replaceAll("(?<!\\\\)\\$" , "§").replace("\\$", "$"));
     }
 }
