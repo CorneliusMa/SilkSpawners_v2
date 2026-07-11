cb8e365e5b89f919bc534926dceadc258e709551
Cornelius <30781324+CorneliusMa@users.noreply.github.com>
2026-07-11 22:41:14 +0200

    refactor(loader): extract reflective class loading into ComponentLoader

---
+++ Plugin/src/main/java/de/corneliusmay/silkspawners/plugin/loader/ComponentLoader.java
@@ -0,0 +1,30 @@
+package de.corneliusmay.silkspawners.plugin.loader;
+
+import java.lang.reflect.Constructor;
+
+public class ComponentLoader<T> {
+
+    private static final String BASE_PACKAGE = "de.corneliusmay.silkspawners";
+
+    private final Class<T> type;
+
+    private final String packagePrefix;
+
+    private final Class<?>[] constructorSignature;
+
+    public ComponentLoader(Class<T> type, String subPackage, Class<?>... constructorSignature) {
+        this.type = type;
+        this.packagePrefix = BASE_PACKAGE + "." + subPackage + ".";
+        this.constructorSignature = constructorSignature;
+    }
+
+    public T instantiate(String relativeName, Object... args) {
+        String className = packagePrefix + relativeName;
+        try {
+            Constructor<? extends T> constructor = Class.forName(className).asSubclass(type).getConstructor(constructorSignature);
+            return constructor.newInstance(args);
+        } catch (ReflectiveOperationException | ClassCastException | IllegalArgumentException e) {
+            throw new ComponentLoadException(className, e);
+        }
+    }
+}
