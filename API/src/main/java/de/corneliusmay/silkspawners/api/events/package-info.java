120cfa8899340661251d4a2e6b5cd81ab6be9a74
Cornelius <30781324+CorneliusMa@users.noreply.github.com>
2026-07-13 01:07:38 +0200

    build(api): enhance javadoc

---
+++ API/src/main/java/de/corneliusmay/silkspawners/api/events/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Custom Bukkit events fired by SilkSpawners, all extending the common base
+ * {@link de.corneliusmay.silkspawners.api.events.SpawnerEvent}. Events are meant to be listened
+ * to, not fired by consumers.
+ */
+package de.corneliusmay.silkspawners.api.events;
