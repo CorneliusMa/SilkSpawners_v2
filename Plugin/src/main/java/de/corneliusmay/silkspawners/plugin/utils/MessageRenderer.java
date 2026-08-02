47c93844516b7a0c4d3ec2331110731673e73849
Spagles <106791090+Spagles@users.noreply.github.com>
2026-08-02 17:01:11 +1000

    fix null pointer exception in MessageRenderer render method

--- Plugin/src/main/java/de/corneliusmay/silkspawners/plugin/utils/MessageRenderer.java
+++ Plugin/src/main/java/de/corneliusmay/silkspawners/plugin/utils/MessageRenderer.java
@@ -34,6 +34,7 @@ public class MessageRenderer {
     }
 
     public static String render(String template, Object... args) {
+        if (template == null) return null;
         if (isMixed(template)) throw new MixedFormattingException();
         // Crowdin only escapes apostrophes in strings containing variables, so plain strings must skip MessageFormat
         if (template.indexOf('§') != -1) return args.length == 0 ? template : MessageFormat.format(template, args);
