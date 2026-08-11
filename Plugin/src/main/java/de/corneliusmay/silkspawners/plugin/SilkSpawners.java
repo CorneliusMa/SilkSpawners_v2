f19730bb6d863d69608ecff831a9672d47fc8108
Cornelius <30781324+CorneliusMa@users.noreply.github.com>
2026-08-11 23:25:12 +0200

    refactor(config): make reload orchestration wired

--- Plugin/src/main/java/de/corneliusmay/silkspawners/plugin/SilkSpawners.java
+++ Plugin/src/main/java/de/corneliusmay/silkspawners/plugin/SilkSpawners.java
@@ -3,16 +3,11 @@ package de.corneliusmay.silkspawners.plugin;
 import de.corneliusmay.silkspawners.plugin.api.SilkSpawnersService;
 import de.corneliusmay.silkspawners.plugin.commands.handler.SilkSpawnersCommand;
 import de.corneliusmay.silkspawners.plugin.commands.handler.SilkSpawnersCommandHandler;
-import de.corneliusmay.silkspawners.plugin.config.ConfigLoader;
 import de.corneliusmay.silkspawners.plugin.config.PluginConfig;
 import de.corneliusmay.silkspawners.plugin.hooks.HookLoader;
-import de.corneliusmay.silkspawners.plugin.locale.LocaleHandler;
 import de.corneliusmay.silkspawners.plugin.utils.Logger;
 import de.corneliusmay.silkspawners.plugin.version.VersionChecker;
-import java.io.IOException;
 import java.time.Duration;
-import java.util.MissingResourceException;
-import java.util.function.BooleanSupplier;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.weftkit.wiring.Registry;
 import org.weftkit.wiring.bukkit.BukkitWeft;
@@ -45,8 +40,7 @@ public class SilkSpawners extends JavaPlugin {
 
     private void registerCommands() {
         SilkSpawnersCommandHandler commandHandler = loader.create(SilkSpawnersCommandHandler.class, "silkspawners");
-        loader.createAll(SilkSpawnersCommand.class, (BooleanSupplier) this::reloadConfiguration)
-                .forEach(commandHandler::addCommand);
+        loader.createAll(SilkSpawnersCommand.class).forEach(commandHandler::addCommand);
         commandHandler.register();
     }
 
@@ -58,19 +52,6 @@ public class SilkSpawners extends JavaPlugin {
         HookLoader hookLoader = loader.get(HookLoader.class);
         hookLoader.addHook("shopguiplus.ShopGUIPlusHook", "ShopGUIPlus", PluginConfig.HOOK_SHOPGUIPLUS);
         hookLoader.register();
-    }
-
-    private synchronized boolean reloadConfiguration() {
-        if (!loader.get(ConfigLoader.class).reload()) return false;
-        try {
-            LocaleHandler localeHandler = loader.get(LocaleHandler.class);
-            if (!localeHandler.isSelectedLocaleLoaded()) localeHandler.loadLocale();
-        } catch (IOException | MissingResourceException ex) {
-            Logger.error("Error loading locale file", ex);
-            return false;
-        }
-        loader.get(VersionChecker.class).restart();
-        return true;
     }
 
     @Override
