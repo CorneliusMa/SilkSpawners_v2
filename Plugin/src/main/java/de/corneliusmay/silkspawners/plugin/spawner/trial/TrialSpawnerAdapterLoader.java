628d60c47ad6a70cb091ba1bfa4e809a2f7a9dd7
Cornelius <30781324+CorneliusMa@users.noreply.github.com>
2026-08-17 22:53:36 +0200

    refactor(version): align dispatch naming

--- Plugin/src/main/java/de/corneliusmay/silkspawners/plugin/spawner/trial/TrialSpawnerAdapterLoader.java
+++ Plugin/src/main/java/de/corneliusmay/silkspawners/plugin/spawner/trial/TrialSpawnerAdapterLoader.java
@@ -1,11 +1,10 @@
 package de.corneliusmay.silkspawners.plugin.spawner.trial;
 
-import static de.corneliusmay.silkspawners.plugin.version.MinecraftVersionChecker.getTrialSpawnerVersion;
-
 import de.corneliusmay.silkspawners.plugin.capability.Capabilities;
 import de.corneliusmay.silkspawners.plugin.capability.Capability;
 import de.corneliusmay.silkspawners.plugin.loader.ComponentLoader;
 import de.corneliusmay.silkspawners.plugin.version.MinecraftVersion;
+import de.corneliusmay.silkspawners.plugin.version.VersionModules;
 import de.corneliusmay.silkspawners.spi.version.TrialSpawnerAdapter;
 import lombok.RequiredArgsConstructor;
 import org.weftkit.wiring.Loader;
@@ -45,7 +44,7 @@ class TrialSpawnerAdapterLoader implements Loader {
     }
 
     private Capability<TrialSpawnerAdapter> loadCapability() {
-        String module = getTrialSpawnerVersion(version);
+        String module = VersionModules.trialSpawner(version);
         if (module == null) return capabilities.unavailable(NAME);
         return capabilities.probe(
                 NAME,
